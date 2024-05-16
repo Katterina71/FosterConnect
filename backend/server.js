c58149673cb8ba43640c757927fed9b7139fb441
Kate Iliu <beviatori@gmail.com>
2024-05-16 09:54:00 -0600

    run local server

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,16 @@ import 'dotenv/config';
 
 //express app
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;;
 
 //routes
 app.get('/', (req, res) => {
     res.json({mssg: 'Welcome to the app'})
 }) 
  
-
-//lister for request
+// listen for request & server error handling
 app.listen(PORT, () => {
-    console.log('Listening on port '+ PORT)
-})
-
+    console.log('Listening on port ' + PORT);
+}).on('error', (err) => {
+    console.error('Error starting server:', err);
+});
