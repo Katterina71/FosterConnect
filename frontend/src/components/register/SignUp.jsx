d894c8d13b95e683e51b5748d5f49ada0025ccca
Kate Iliu <beviatori@gmail.com>
2024-05-30 12:23:36 -0600

    Added form Error Handling

--- frontend/src/components/register/SignUp.jsx
+++ frontend/src/components/register/SignUp.jsx
@@ -5,13 +5,28 @@ import { useAuth } from '../../context/AuthContext';
 function SignUp() {
   const emailRef = useRef()
   const passwordRef = useRef()
+  const passwordConfirmRef = useRef()
   const {signup} = useAuth()
+
+  //Validation and Handle errors
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
 
  async function handleClick(e) {
   e.preventDefault()
-  console.log('hello')
-  console.log(emailRef.current.value, passwordRef.current.value)
-  return await signup(emailRef.current.value, passwordRef.current.value)
+  console.log(emailRef.current.value, passwordRef.current.value) // Debugging
+
+  if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+    return setError('Passwords do not match');
+}
+  try {
+        setError('');
+        setLoading(true);
+        await signup(emailRef.current.value, passwordRef.current.value);
+  } catch (error) {
+    setError('Failed to create an account');
+  }
+    setLoading(false);
   }
 
 
@@ -26,11 +41,20 @@ function SignUp() {
         ref = {emailRef}
         />
       <input 
+        required
         name="password"
         label="Password"
         type="password"
         id="password"
         ref = {passwordRef}
+      />
+      <input 
+        required
+        name="password-confirm"
+        label="Password Confirmation"
+        type="password"
+        id="password-confirm"
+        ref = {passwordConfirmRef}
       />
       <button onClick = {(e) => handleClick(e)}>Submit</button>
     </div>
