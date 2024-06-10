24e36e08e7d215ed1c55a0c989150d393eb7cf1c
Kate Iliu <beviatori@gmail.com>
2024-06-10 15:57:33 -0600

    Added switch between pets and shelters

--- frontend/src/pages/Finder.jsx
+++ frontend/src/pages/Finder.jsx
@@ -1,14 +1,28 @@
-import { Typography, Box, Container } from "@mui/material"
+import { useState } from "react"
+import { Typography, Box, Container, FormControlLabel } from "@mui/material"
 import Pets from "../components/finder/Pets"
 import Shelters from "../components/finder/Shelters"
+import MaterialUISwitch from "../components/forms/profile_forms/MaterialUISwitch"
 
 export default function Finder() {
+    const [checked, setChecked] = useState(true)
+
+    const handleChange = (e) => {
+        const newChecked = e.target.checked
+        setChecked(newChecked);
+    }
+
   return (
         <Box sx={{mt:4}}>
             <Container>
                 <Typography variant="h1">Find a pet or shelter</Typography>
-                <Pets/>
-                <Shelters />
+                <FormControlLabel
+                    control={<MaterialUISwitch checked={checked} sx={{ m: 1 }} />}
+                    label={checked ? 'Pets Finder' : 'Shelter Finder'} 
+                    onChange={handleChange}
+                 />
+               {checked ? <Pets/> : <Shelters /> } 
+             
             </Container>
         </Box>
   )
