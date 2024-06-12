be5c32147d98dab5c2c6b007fb35d347d15e61e2
Kate Iliu <beviatori@gmail.com>
2024-06-12 16:25:00 -0600

    Deploy

--- frontend/src/services/endpointsDB.js
+++ frontend/src/services/endpointsDB.js
@@ -1,7 +1,9 @@
 // export const BASE_URL = import.meta.env.DEV ? 'http://localhost:5050/api/users' : 'https://fullstackapp-gfyd.onrender.com/api/todos'
-const BASE_URL =import.meta.env.DEV ? 'http://localhost:5050/api/users' : 'https://fosterconnect-server.onrender.com/api/users';
+// const BASE_URL =import.meta.env.DEV ? 'http://localhost:5050/api/users' : 'https://fosterconnect-server.onrender.com/api/users';
+// const BASE_PETS_URL =import.meta.env.DEV ? 'http://localhost:5050/api/pets': 'https://fosterconnect-server.onrender.com/api/pets';
 
-const BASE_PETS_URL =import.meta.env.DEV ? 'http://localhost:5050/api/pets': 'https://fosterconnect-server.onrender.com/api/pets';
+const BASE_URL ='https://fosterconnect-server.onrender.com/api/users';
+const BASE_PETS_URL ='https://fosterconnect-server.onrender.com/api/pets';
 
 export {BASE_URL, BASE_PETS_URL}
 
